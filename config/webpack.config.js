a6c40cb8ca39991055a349104143fac85ec95584
jingchunbo <chunbo.jing@cyclone-robotics.com>
2021-04-02 15:41:52 +0800

    去掉css报错

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -120,8 +120,6 @@ module.exports = function (webpackEnv) {
           ident: 'postcss',
           plugins: () => [
             require('autoprefixer'),
-            require('tailwindcss'),
-            require('postcss-flexbugs-fixes'),
             require('postcss-preset-env')({
               autoprefixer: {
                 flexbox: 'no-2009',
